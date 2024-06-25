62dcf0c00c23fe713be7bbe1506bc82681cc6282
Sakshi <sakshigupta0513@gmail.com>
2024-06-25 23:14:22 +0530

    feat: updated thunk

--- src/services/getProductsSlice.js
+++ src/services/getProductsSlice.js
@@ -2,8 +2,8 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 
 export const fetchProducts = createAsyncThunk(
     'content/fetchProducts',
-    async () => {
-        const response = await fetch(`https://fakestoreapi.com/products`)
+    async (url) => {
+        const response = await fetch(url)
         const data = await response.json()
       return data
     }
